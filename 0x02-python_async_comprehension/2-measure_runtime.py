189cd723f48ea62c4eaa02f865ba1d8362bd14c8
ofentse loeto <ohentseloeto610@gmail.com>
2023-10-10 21:41:12 +0000

    Measure the total runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,21 +4,8 @@ write a measure_runtime coroutine that will execute
 async_comprehension four times in parallel using asyncio.gather.
 """
 import asyncio
-from typing import List
-
-
-async def async_comprehension() -> List[float]:
-    """
-    This function creates async_comprehension coroutine that
-    collects collects 10 random numbers using an asynchronous
-    comprehension over the async_generator coroutine
-
-    Returns:
-        A list of collected numbers.
-    """
-
-    collected_numbers = [i async for i in async_generator()]
-    return collected_numbers
+import time
+from async_comprehensions import async_comprehension
 
 
 async def measure_runtime() -> float:
@@ -27,14 +14,13 @@ async def measure_runtime() -> float:
     async_comprehension file.
     It then uses asyncio.gather to execute asyns_comprehension
     four times concurrently.
-    It measures the total runtime using
-    asyncio.get_event_loop().time()
+    It measures the total runtime using time.time()
 
     Returns:
         The total runtime.
     """
 
-    start_time = asyncio.get_event_loop().time()
+    start_time = time.time()
 
     # Execute async_comprehension four times in parallel
     await asyncio.gather(
@@ -44,7 +30,7 @@ async def measure_runtime() -> float:
             async_comprehension()
     )
 
-    end_time = asyncio.get_event_loop().time()
+    end_time = time.time()
     total_runtime = end_time - start_time
 
     return total_runtime
